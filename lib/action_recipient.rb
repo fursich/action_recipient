40692c243e5009fd94abdd10c4550f6e3525386a
Koji Onishi <fursich0@gmail.com>
2019-07-26 13:08:02 +0900

    initial commit

---
+++ lib/action_recipient.rb
@@ -0,0 +1,6 @@
+require "action_recipient/version"
+
+module ActionRecipient
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/action_recipient/version.rb
@@ -0,0 +1,3 @@
+module ActionRecipient
+  VERSION = "0.1.0"
+end
