9e8b4a9bbf08e70d1bdcf2408ad3e69fe37cc987
Koji Onishi <fursich0@gmail.com>
2020-12-26 19:57:03 +0900

    test against Ruby3 and latest patch versions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,12 @@ require 'action_recipient'
 require 'mail'
 require 'pry'
 
+if RUBY_VERSION >= '2.7.2'
+  # NOTE: https://bugs.ruby-lang.org/issues/17000
+  # this will keep us informed of deprecation warnings after Ruby 2.7.2
+  Warning[:deprecated] = true
+end
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
